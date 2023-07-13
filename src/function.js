ed3b70f4fb9f74206bf83849952f62108673b1e6
G-Alessandro <galloalessandro77@gmail.com>
2023-07-13 13:54:32 +0200

    Added weatherAPI information of the current day to container1

--- src/function.js
+++ src/function.js
@@ -1,6 +1,34 @@
-fetch('http://api.weatherapi.com/v1/current.json?key=008478c79be54c9d8e9123022230607&q=london')
-  .then((response) => {
-    console.log(response.json());
-  });
+const location = 'rome';
 
-console.log('test');
+fetch(`http://api.weatherapi.com/v1/forecast.json?key=008478c79be54c9d8e9123022230607&q=${location}&days=7`, { mode: 'cors' })
+  .then((response) => console.log(response.json()));
+
+function weather() {
+  fetch(`http://api.weatherapi.com/v1/forecast.json?key=008478c79be54c9d8e9123022230607&q=${location}&days=7`, { mode: 'cors' })
+    .then((response) => response.json())
+    .then((response) => {
+      const locationName = document.getElementById('locationName');
+      const locationRegion = document.getElementById('locationRegion');
+      const locationCountry = document.getElementById('locationCountry');
+      const lastUpdated = document.getElementById('lastUpdated');
+      const temperature = document.getElementById('temperature');
+      const todayWeather = document.getElementById('todayWeather');
+      const todayWeatherImg = document.getElementById('todayWeatherImg');
+      const todayFeelsLike = document.getElementById('todayFeelsLike');
+      const todayHumidity = document.getElementById('todayHumidity');
+      const todayWindSpeed = document.getElementById('todayWindSpeed');
+
+      locationName.innerText = response.location.name;
+      locationRegion.innerText = response.location.region;
+      locationCountry.innerText = response.location.country;
+      lastUpdated.innerText = response.current.last_updated;
+      temperature.innerText = response.current.temp_c;
+      todayWeather.innerText = response.current.condition.text;
+      todayWeatherImg.src = response.current.condition.icon;
+      todayFeelsLike.innerText = `${response.current.feelslike_c} ºC`;
+      todayHumidity.innerText = `${response.current.humidity} %`;
+      todayWindSpeed.innerText = `${response.current.wind_kph} km/h`;
+    });
+}
+
+export { weather };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,118 @@
 import './style.css';
-import './function'
+import searchIcon from './images/magnify.svg';
+import { weather } from './function';
+
+const body = document.getElementsByTagName('body')[0];
+
+// const imgTest = document.createElement('img');
+// body.appendChild(imgTest);
+// imgTest.src = img;
+
+// fetch('http://api.weatherapi.com/v1/current.json?key=008478c79be54c9d8e9123022230607&q=guidonia', { mode: 'cors' })
+//   .then((response) => response.json())
+//   .then((response) => {
+//     // console.log(response.current.cloud);
+//     imgTest.src = response.current.condition.icon;
+//   });
+
+// Title
+const titleApp = document.createElement('div');
+titleApp.classList.add('title-app');
+titleApp.innerText = 'WEATHER APP';
+body.appendChild(titleApp);
+
+// Container for weather of the day
+const container1 = document.createElement('div');
+container1.classList.add('container-1');
+body.appendChild(container1);
+
+const locationName = document.createElement('div');
+locationName.setAttribute('id', 'locationName');
+container1.appendChild(locationName);
+
+const locationRegion = document.createElement('div');
+locationRegion.setAttribute('id', 'locationRegion');
+container1.appendChild(locationRegion);
+
+const locationCountry = document.createElement('div');
+locationCountry.setAttribute('id', 'locationCountry');
+container1.appendChild(locationCountry);
+
+const lastUpdated = document.createElement('div');
+lastUpdated.setAttribute('id', 'lastUpdated');
+container1.appendChild(lastUpdated);
+
+const temperature = document.createElement('div');
+temperature.setAttribute('id', 'temperature');
+container1.appendChild(temperature);
+
+const form = document.createElement('form');
+container1.appendChild(form);
+
+const inputLocation = document.createElement('input');
+inputLocation.setAttribute('type', 'text');
+inputLocation.setAttribute('placeholder', 'Enter City Name');
+form.appendChild(inputLocation);
+
+const submitBtn = document.createElement('button');
+form.appendChild(submitBtn);
+const submitBtnImg = document.createElement('img');
+submitBtnImg.src = searchIcon;
+submitBtn.appendChild(submitBtnImg);
+
+const temperatureChangeBtn = document.createElement('button');
+temperatureChangeBtn.setAttribute('id', 'temperatureChangeBtn');
+container1.appendChild(temperatureChangeBtn);
+
+const todayWeather = document.createElement('div');
+todayWeather.setAttribute('id', 'todayWeather');
+container1.appendChild(todayWeather);
+
+const todayWeatherImg = document.createElement('img');
+todayWeatherImg.setAttribute('id', 'todayWeatherImg');
+container1.appendChild(todayWeatherImg);
+
+// Today Weather container
+const todayFeelsLikeContainer = document.createElement('div');
+container1.appendChild(todayFeelsLikeContainer);
+
+const todayFeelsLikeTitle = document.createElement('div');
+todayFeelsLikeTitle.innerText = 'Feels Like';
+todayFeelsLikeContainer.appendChild(todayFeelsLikeTitle);
+
+const todayFeelsLike = document.createElement('div');
+todayFeelsLike.setAttribute('id', 'todayFeelsLike');
+todayFeelsLikeContainer.appendChild(todayFeelsLike);
+
+// Today Humidity container
+const todayHumidityContainer = document.createElement('div');
+container1.appendChild(todayHumidityContainer);
+
+const todayHumidityTitle = document.createElement('div');
+todayHumidityTitle.innerText = 'Humidity';
+todayHumidityContainer.appendChild(todayHumidityTitle);
+
+const todayHumidity = document.createElement('div');
+todayHumidity.setAttribute('id', 'todayHumidity');
+todayHumidityContainer.appendChild(todayHumidity);
+
+// Today Wind Speed container
+const todayWindSpeedContainer = document.createElement('div');
+container1.appendChild(todayWindSpeedContainer);
+
+const todayWindSpeedTitle = document.createElement('div');
+todayWindSpeedTitle.innerText = 'Wind Speed';
+todayWindSpeedContainer.appendChild(todayWindSpeedTitle);
+
+const todayWindSpeed = document.createElement('div');
+todayWindSpeed.setAttribute('id', 'todayWindSpeed');
+todayWindSpeedContainer.appendChild(todayWindSpeed);
+
+// Container for weather forecasts for the next 7 days / 24 hours
+const container2 = document.createElement('div');
+container2.classList.add('container-2');
+body.appendChild(container2);
+
+weather();
+
+console.log('test');
